93c363b4ec0c3ba3586581da811eccc7d806efef
Dewhs <chevreuil.chauve@gmail.com>
2025-07-01 00:59:13 +0200

    fix: Button -> MButtonPrimary
    this name can evolved to just MButton. But for now I choose to named Primary to refactor later (if we need to) when we will need others types of button

--- android/app/src/main/java/com/example/makeat/ui/LoginPage.kt
+++ android/app/src/main/java/com/example/makeat/ui/LoginPage.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,10 +18,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.example.makeat.ui.components.MButtonPrimary
 import com.example.makeat.ui.components.MCheckbox
 import com.example.makeat.ui.components.MTextField
-import sv.lib.squircleshape.CornerSmoothing
-import sv.lib.squircleshape.SquircleShape
 
 @Composable
 fun LoginPage(modifier: Modifier = Modifier) {
@@ -81,23 +78,7 @@ fun LoginPage(modifier: Modifier = Modifier) {
                 Text("I don't have an account", style = MaterialTheme.typography.bodyMedium)
             }
 
-            Button(
-                onClick = { /* TODO: Action login */ },
-                shape = SquircleShape(
-                    radius = 25.dp,
-                    cornerSmoothing = CornerSmoothing.Full
-                ),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary
-                ), modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(
-                    "Enter",
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.padding(vertical = 10.dp)
-                )
-            }
+            MButtonPrimary(text = "Enter", onClick = { TODO() /* Action login */ })
         }
     }
 }
